0cb3f842e84dfcfdd31a199341e5a26c2cd259a5
EddieXie <eddie.xie@gmail.com>
2023-07-28 23:01:30 +0800

    speed adjust

--- HiWonder.py
+++ HiWonder.py
@@ -22,7 +22,8 @@ window = pygame.display.set_mode((300,300))
 pygame.display.set_caption("Pygame Demonstration")
 
 mainloop=True
-a=10
+s=20
+a=s
 control = [0, 0, 0, 0]
 while mainloop:
 
@@ -33,35 +34,38 @@ while mainloop:
             mainloop=False
         
         if event.type==pygame.KEYDOWN:
-            print(event.key, " ", a)
-
-            if event.key==264:#UP
+            print(event.key, ", ", a, ", ", s)
+            control = [0, 0, 0, 0]
+            if event.key==264 or event.key==119:#UP w
                 control = [a, a, a, a]
-            if event.key==258:#DOWN
+            if event.key==258 or event.key==120:#DOWN x
                 control = [-a, -a, -a, -a]
-            if event.key==260:#LEFT
+            if event.key==260 or event.key==97:#LEFT a
                 control = [-a, a, a, -a]
-            if event.key==262:#RIGHT
+            if event.key==262 or event.key==100:#RIGHT d
                 control = [a, -a, -a, a]
-            if event.key==263:#HOME
+            if event.key==263 or event.key==113:#HOME q
                 control = [0, a, a, 0]
-            if event.key==259:#PAGE DOWN
+            if event.key==259 or event.key==99:#PAGE DOWN c
                 control = [0, -a, -a, 0]
-            if event.key==265:#PAGE UP
+            if event.key==265 or event.key==101:#PAGE UP e
                 control = [a, 0, 0, a]
-            if event.key==257:#END
+            if event.key==257 or event.key==122:#END z
                 control = [-a, 0, 0, -a]
-            if event.key==267:#/
+            if event.key==267 or event.key==91:#/ [
                 control = [-a, a, -a, a]
-            if event.key==268:#*
-                control = [a, -a, a, -a]
-    
-            bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, control)
+            if event.key==268 or event.key==93:#* ]
+                control = [a, -a, a, -a]            
+            if event.key==270 or event.key==61:#+
+                s += 5
+            if event.key==269 or event.key==45:#-
+                s -= 5            
 
 
         if event.type==pygame.KEYUP:
-            bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, [0, 0, 0, 0])
-            a=10
+            control = [0, 0, 0, 0]
+            a=s
+        bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, control)
 
 pygame.quit()
   

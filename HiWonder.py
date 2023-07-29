cbd75946033fe9e672436a089fb2cffc1fbbd3e9
EddieXie <eddie.xie@gmail.com>
2023-07-29 13:46:04 +0800

    to support adjust speed

--- HiWonder.py
+++ HiWonder.py
@@ -14,6 +14,7 @@ MOTOR_TYPE_JGB37_520_12V_110RPM = 3
 bus = SMBus(1)
 
 bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_TYPE_ADDR, MOTOR_TYPE_JGB37_520_12V_110RPM)
+bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_ENCODER_POLARITY_ADDR, 0)
 b = bus.read_word_data(CAM_DEFAULT_I2C_ADDRESS, ADC_BAT_ADDR)
 print("BAT:", b)
 
@@ -71,6 +72,7 @@ while mainloop:
             a=s
             print(event.key, ", ", a, ", ", s, ", ", control)
             bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, control)
+            
         
 
 pygame.quit()

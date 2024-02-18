d7918c224aba4278dab88acb2f85529dc94e832a
Eddie Xie <eddie.xie@gmail.com>
2024-02-18 18:56:33 +0800

    control by axis

--- HiWonder-joystick.py
+++ HiWonder-joystick.py
@@ -1,3 +1,5 @@
+import math
+from numpy import interp
 from smbus2 import SMBus
 import pygame
 
@@ -11,10 +13,12 @@ ADC_BAT_ADDR = 0
 MOTOR_TYPE_JGB37_520_12V_110RPM = 3
 
 bus = SMBus(1)
+bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_TYPE_ADDR, MOTOR_TYPE_JGB37_520_12V_110RPM)
+bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_ENCODER_POLARITY_ADDR, 0)
+bat = bus.read_word_data(CAM_DEFAULT_I2C_ADDRESS, ADC_BAT_ADDR)
+print("BAT:", bat)
 
 pygame.init()
-deadZone = {'axis0':0.1, 'axis1':0.1, 'axis2':0.1, 'axis3':0.1, 'axis4':0.1, 'axis5':0.1}
-center = {'axis0':0.0, 'axis1':0.0, 'axis2':0.0, 'axis3':0.0, 'axis4':-1.0, 'axis5':-1.0}
 
 # This is a simple class that will help us print to the screen.
 # It has nothing to do with the joysticks, just outputting the
@@ -40,10 +44,17 @@ class TextPrint:
     def unindent(self):
         self.x -= 10
 
+deadZone=0.2
+def map_motor(input):
+    if input > deadZone:
+        return round(interp(input, [deadZone, 1], [0, 100]))
+    if input < -deadZone:
+        return round(interp(input, [-1, -deadZone], [-100, 0]))
+    return 0
 
 def main():
     # Set the width and height of the screen (width, height), and name the window.
-    screen = pygame.display.set_mode((500, 700))
+    screen = pygame.display.set_mode((800, 800))
     pygame.display.set_caption("Joystick example")
 
     # Used to manage how fast the screen updates.
@@ -59,15 +70,11 @@ def main():
 
     done = False
     while not done:
-        # Event processing step.
-        # Possible joystick events: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
-        # JOYBUTTONUP, JOYHATMOTION, JOYDEVICEADDED, JOYDEVICEREMOVED
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True  # Flag that we are done so we exit this loop.
 
             if event.type == pygame.JOYBUTTONDOWN:
-                print("Joystick button pressed.")
                 if event.button == 0:
                     joystick = joysticks[event.instance_id]
                     if joystick.rumble(0, 0.7, 500):
@@ -76,10 +83,7 @@ def main():
             if event.type == pygame.JOYBUTTONUP:
                 print("Joystick button released.")
 
-            # Handle hotplugging
             if event.type == pygame.JOYDEVICEADDED:
-                # This event will be generated when the program starts for every
-                # joystick, filling up the list without needing to create them manually.
                 joy = pygame.joystick.Joystick(event.device_index)
                 joysticks[joy.get_instance_id()] = joy
                 print(f"Joystick {joy.get_instance_id()} connencted")
@@ -88,67 +92,60 @@ def main():
                 del joysticks[event.instance_id]
                 print(f"Joystick {event.instance_id} disconnected")
 
-        # Drawing step
-        # First, clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
-        screen.fill((255, 255, 255))
         text_print.reset()
 
-        # Get count of joysticks.
         joystick_count = pygame.joystick.get_count()
 
-        text_print.tprint(screen, f"Number of joysticks: {joystick_count}")
-        text_print.indent()
+        if joystick_count == 0:
+            continue
+        joystick = joysticks[0]
+        
+        x = joystick.get_axis(0) * 0.8
+        y = -joystick.get_axis(1)
+        turn = joystick.get_axis(2) * 0.7
+        
+        theta = math.atan2(y, x)
+        power = math.hypot(x, y)
+        
+        sin = math.sin(theta - math.pi/4)
+        cos = math.cos(theta - math.pi/4)
+        maximum = max(abs(sin), abs(cos))
+        
+        leftFront = power * cos / maximum + turn
+        rightFront = power * sin / maximum - turn
+        leftRear = power * sin / maximum + turn
+        rightRear = power * cos / maximum - turn
+        
+        if power + abs(turn) > 1:
+            leftFront /= power + turn
+            rightFront /= power - turn
+            leftRear /= power + turn
+            rightRear /= power - turn
+            
+            
+        motor = [map_motor(leftFront), map_motor(rightFront), map_motor(leftRear), map_motor(rightRear)]
 
-        # For each joystick:
-        for joystick in joysticks.values():
-            jid = joystick.get_instance_id()
+        screen.fill((round(interp(x, [-100, 100], [50, 255])), round(interp(y, [-100, 100], [50, 255])), round(interp(turn, [-100, 100], [50, 255]))))
+        
+        text_print.tprint(screen, f"X: {x}")
+        text_print.tprint(screen, f"Y: {y}")
+        text_print.tprint(screen, f"Turn: {turn}")
+        text_print.tprint(screen, f"Theta: {theta}")
+        text_print.tprint(screen, f"Power: {power}")
+        text_print.tprint(screen, f"Sin: {sin}")
+        text_print.tprint(screen, f"Cos: {cos}")
+        text_print.tprint(screen, f"LeftFront: {leftFront}")
+        text_print.tprint(screen, f"RightFront: {rightFront}")
+        text_print.tprint(screen, f"LeftRear: {leftRear}")
+        text_print.tprint(screen, f"LeftRear: {rightRear}")
+        text_print.tprint(screen, f"Motor: {motor}")
 
-            text_print.tprint(screen, f"Joystick {jid}")
-            text_print.indent()
+        pygame.draw.circle(screen, [0, 0, 200], [400, 400], 300, width = 5)
+        pygame.draw.circle(screen, [200, 0, 0], [400 + math.cos(theta) * power * 200, 400 - math.sin(theta) * power * 200], 50)
+        #pygame.draw.circle(screen, [200, 0, 0], [math.theta, 400], power * 100)
+        
 
-            # Get the name from the OS for the controller/joystick.
-            name = joystick.get_name()
-            text_print.tprint(screen, f"Joystick name: {name}")
-
-            guid = joystick.get_guid()
-            text_print.tprint(screen, f"GUID: {guid}")
-
-            power_level = joystick.get_power_level()
-            text_print.tprint(screen, f"Joystick's power level: {power_level}")
-
-            # Usually axis run in pairs, up/down for one, and left/right for
-            # the other. Triggers count as axes.
-            axes = joystick.get_numaxes()
-            text_print.tprint(screen, f"Number of axes: {axes}")
-            text_print.indent()
-
-            for i in range(axes):
-                axis = joystick.get_axis(i)
-                text_print.tprint(screen, f"Axis {i} value: {axis:>6.3f}")
-            text_print.unindent()
-
-            buttons = joystick.get_numbuttons()
-            text_print.tprint(screen, f"Number of buttons: {buttons}")
-            text_print.indent()
-
-            for i in range(buttons):
-                button = joystick.get_button(i)
-                text_print.tprint(screen, f"Button {i:>2} value: {button}")
-            text_print.unindent()
-
-            hats = joystick.get_numhats()
-            text_print.tprint(screen, f"Number of hats: {hats}")
-            text_print.indent()
-
-            # Hat position. All or nothing for direction, not a float like
-            # get_axis(). Position is a tuple of int values (x, y).
-            for i in range(hats):
-                hat = joystick.get_hat(i)
-                text_print.tprint(screen, f"Hat {i} value: {str(hat)}")
-            text_print.unindent()
-
-            text_print.unindent()
+        bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, motor)
 
         # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
@@ -159,6 +156,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # If you forget this line, the program will 'hang'
-    # on exit if running from IDLE.
     pygame.quit()

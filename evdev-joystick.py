e069c729ec6a0392da2a872afe12e217b38566b1
Eddie Xie <eddie.xie@gmail.com>
2024-05-11 21:23:56 +0800

    evdev

--- evdev-joystick.py
+++ evdev-joystick.py
@@ -2,6 +2,8 @@
 import evdev
 import math
 import numpy as np
+import time
+from evdev import categorize, ecodes, KeyEvent
 from numpy import interp
 from smbus2 import SMBus
 
@@ -20,21 +22,27 @@ bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_ENCODER_POLARITY_ADDR, 0)
 bat = bus.read_word_data(CAM_DEFAULT_I2C_ADDRESS, ADC_BAT_ADDR)
 print("BAT:", bat)
 
-def proecss(event):
-    if joystick.get_button(3):
-        bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, [0, 0, 0, 0])
-    else:
-        bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, motor)
+def process(event, x):
+    if event.type == ecodes.EV_ABS:
+        e = categorize(event)
+        print(x, e.event.code, e.event.value)
+
+        if x%3 == 0:
+            bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, [0, -0, -0, 0])
+        else:
+            bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, [10, -10, -10, 10])
 
 def main():
+    x=0
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
     for device in devices:
         print(device.path, device.name, device.phys)
         if 'Xbox Wireless Controller' != device.name:
             continue
         for event in device.read_loop():
-            process(event)
-
+            process(event, x)
+            time.sleep(1)
+            x+=1
 
 if __name__ == "__main__":
     main()

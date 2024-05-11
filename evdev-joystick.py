a711dfd21b27a144a27618ab91e4aa0a041972ed
eddiexie <eddie_xie@derbysoft.com>
2024-05-11 10:48:42 +0800

    joystick

---
+++ evdev-joystick.py
@@ -0,0 +1,40 @@
+# python -m evdev.evtest
+import evdev
+import math
+import numpy as np
+from numpy import interp
+from smbus2 import SMBus
+
+CAM_DEFAULT_I2C_ADDRESS = 0x34
+MOTOR_TYPE_ADDR = 20
+MOTOR_FIXED_SPEED_ADDR = 51
+MOTOR_ENCODER_POLARITY_ADDR = 21
+MOTOR_FIXED_PWM_ADDR = 31
+MOTOR_ENCODER_TOTAL_ADDR = 60
+ADC_BAT_ADDR = 0
+MOTOR_TYPE_JGB37_520_12V_110RPM = 3
+
+bus = SMBus(1)
+bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_TYPE_ADDR, MOTOR_TYPE_JGB37_520_12V_110RPM)
+bus.write_byte_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_ENCODER_POLARITY_ADDR, 0)
+bat = bus.read_word_data(CAM_DEFAULT_I2C_ADDRESS, ADC_BAT_ADDR)
+print("BAT:", bat)
+
+def proecss(event):
+    if joystick.get_button(3):
+        bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, [0, 0, 0, 0])
+    else:
+        bus.write_i2c_block_data(CAM_DEFAULT_I2C_ADDRESS, MOTOR_FIXED_SPEED_ADDR, motor)
+
+def main():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    for device in devices:
+        print(device.path, device.name, device.phys)
+        if 'Xbox Wireless Controller' != device.name:
+            continue
+        for event in device.read_loop():
+            process(event)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Pair with XBox controller
+    bluetoothctrl
+        scan on
+        pair EC:83:50:8E:A6:78
+        connect EC:83:50:8E:A6:78
